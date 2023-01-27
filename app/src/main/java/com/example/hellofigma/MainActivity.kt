d0408b7c360383e0dedead37353c47fe30729436
alexeses <46347629+alexeses@users.noreply.github.com>
2023-01-27 16:21:51 +0100

    add proofs

--- app/src/main/java/com/example/hellofigma/MainActivity.kt
+++ app/src/main/java/com/example/hellofigma/MainActivity.kt
@@ -19,13 +19,12 @@ package com.example.hellofigma
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.hellofigma.hellocard.HelloCard
 import com.example.hellofigma.ui.theme.HelloFigmaTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,12 +32,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HelloFigmaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colors.background) {
+                    HelloCard()
                 }
             }
         }
@@ -54,6 +49,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     HelloFigmaTheme {
-        Greeting("Android")
+        HelloCard()
     }
 }
